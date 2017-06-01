100cc2b910edc9dfd73a75e9143c30ab0470f874
mivanchenko <mivanchenko@cfmc.ru>
2017-06-01 18:28:22 +0300

    2017-06-01 sp_basin finished created quotas schema tb_basin -> into quotas schema

--- db_quotas_2018/PostgreSQL/stored_procedures/sp_basin.sql
+++ db_quotas_2018/PostgreSQL/stored_procedures/sp_basin.sql
@@ -4,115 +4,190 @@
 -- PostgreSQL
 -- Автор: Иванченко М.В.
 -- Дата начала разработки:  31-05-2017
--- Дата обновления:         31-05-2017
+-- Дата обновления:         01-06-2017
 -- Первый релиз:            0.0.0.0
 -- Текущий релиз:           0.0.0.0
 -- =============================================================================
 /*
-    public.sp_basin_read( ... ) 
+    quotas.sp_basin_read( p_id INTEGER ) 
+    - функции сервера для выборки ОДНОЙ записи данных бассейна промысла
+    
+    параметры:
+        p_id - идентификатор бассейна промысла
+        
+    возвращает:
+        -- запись данных бассейна промысла
+*/
+--
+DROP FUNCTION IF EXISTS quotas.sp_basin_read(INTEGER);
+--
+CREATE OR REPLACE FUNCTION quotas.sp_basin_read( p_id INTEGER ) 
+RETURNS quotas.tb_basin
+AS
+$$
+    SELECT  quotas.tb_basin.id_basin, 
+    		 quotas.tb_basin.basin, 
+             quotas.tb_basin.basin_abbr
+        FROM quotas.tb_basin
+        WHERE id_basin=p_id LIMIT 1 OFFSET 0;
+$$
+LANGUAGE sql VOLATILE
+COST 100;
+/*
+    quotas.sp_basin_read( ) 
+    - функции сервера для выборки ВСЕХ записей данных бассейнов промысла
+    
+    возвращает:
+        -- набор записей данных бассейна промысла
+*/
+-- 
+DROP FUNCTION IF EXISTS quotas.sp_basin_read( );
+-- 
+CREATE OR REPLACE FUNCTION quotas.sp_basin_read( ) 
+RETURNS SETOF quotas.tb_basin 
+AS
+$$
+	SELECT id_basin, basin, basin_abbr 
+	    FROM quotas.tb_basin;
+$$ 
+LANGUAGE sql VOLATILE
+COST 100;
+/*TESTS*/
+-- select * from sp_basin_read(63)
+-- select quotas.sp_basin_read( 0 );
+-- select quotas.sp_basin_read( );
+-- select * from quotas.sp_basin_read( );
+
+/*
+    quotas.sp_basin( ... ) 
     - функции сервера для работы с данными бассейнов промысла
     
     параметры:
-        p_year_from - год начала периода данных выборки
-        p_year_to   - год окончания периода данных выборки
-        
-    вызывает исключение в случаях:
-        - Год начала периода ранее 2004 года или младше текущего года
-        - Год окончания периода старше начала периода или младше текущего года
-        
-    возвращает:
-        -- данных объёма улова за период заданных годов
+        p_action - код действия с записью БД
+                    'CREATE' - вставка, 
+                    'PATCH' - обновление, 
+                    'REMOVE' - удаление
+        p_id - код записи БД id_basin, для операции вставки не используется
+        p_basin - значение наименования бассейна промысла
+        p_abbr - значение аббревиатуры наименования бассейна промысла
+    возвращает:    
+        идентификатор вставленной/обновлённой/удалённой записи
 */
 --
-DROP FUNCTION IF EXISTS public.sp_basin_read(INTEGER);
+DROP FUNCTION IF EXISTS quotas.sp_basin(VARCHAR,INTEGER,VARCHAR,VARCHAR);
 --
-CREATE OR REPLACE FUNCTION public.sp_basin_read( 
-                                                p_id INTEGER DEFAULT 0
-                                               ) 
-RETURNS TABLE
-    (
-        i_id INTEGER,
-        x_basin VARCHAR(32), 
-        x_basin_abbr VARCHAR(4)
-    )
+CREATE OR REPLACE FUNCTION quotas.sp_basin( 
+                                            p_action VARCHAR(8) DEFAULT '',
+                                            p_id INTEGER DEFAULT 0,
+                                            p_basin VARCHAR(32) DEFAULT '',
+                                            p_abbr VARCHAR(4) DEFAULT ''
+                                          ) 
+RETURNS INTEGER
 AS
 $$
+    DECLARE result_id INTEGER;
 BEGIN
-    -- выборка всех данных
-    IF  p_id=0 THEN
-        RETURN QUERY
-            SELECT id_basin, basin, basin_abbr 
-                FROM public.tb_basin;
-    END IF;
-        
-    RETURN QUERY
-        SELECT id_basin, basin, basin_abbr 
-            FROM public.tb_basin
-        WHERE id_basin=p_id;
-
+    -- insert new basin
+    IF p_action='CREATE' THEN
+    BEGIN
+        IF EXISTS(SELECT * FROM quotas.tb_basin) THEN
+            BEGIN
+                INSERT INTO quotas.tb_basin
+                    SELECT MAX(id_basin)+1, p_basin, p_abbr
+                    FROM quotas.tb_basin
+                RETURNING id_basin INTO result_id;
+                RETURN result_id;
+            END;
+        ELSE
+            BEGIN
+                INSERT INTO quotas.tb_basin
+                    VALUES(1, p_basin, p_abbr);
+                RETURN 1;
+            END;
+        END IF;
+    END;    
+    END IF; 
+    -- update basin
+    IF p_action='PATCH' THEN
+        BEGIN
+            UPDATE quotas.tb_basin 
+                SET basin=p_basin, basin_abbr=p_abbr
+                WHERE id_basin=p_id
+            RETURNING id_basin INTO result_id;
+            RETURN result_id;
+        END;
+    END IF; 
+    -- delete basin
+    IF p_action='REMOVE' THEN
+    	BEGIN
+            DELETE FROM quotas.tb_basin 
+                WHERE id_basin=p_id
+            RETURNING id_basin INTO result_id;
+            RETURN result_id;
+        END;
+    END IF; 
 END;
 $$ LANGUAGE plpgsql;
 
 /*
-    public.sp_basin( ... ) 
-    - функции сервера для работы с данными бассейнов промысла
-    
-    параметры:
-        p_year_from - год начала периода данных выборки
-        p_year_to   - год окончания периода данных выборки
-        
-    вызывает исключение в случаях:
-        - Год начала периода ранее 2004 года или младше текущего года
-        - Год окончания периода старше начала периода или младше текущего года
-        
-    возвращает:
-        -- данных объёма улова за период заданных годов
-*/
---
-DROP FUNCTION IF EXISTS public.sp_basin(VARCHAR,INTEGER,VARCHAR,VARCHAR);
---
-CREATE OR REPLACE FUNCTION public.sp_basin( 
-                                            p_action VARCHAR(8),
-                                            p_id INTEGER,
-                                            p_basin VARCHAR(32),
-                                            p_abbr VARCHAR(4)
+CREATE OR REPLACE FUNCTION quotas.sp_basin( 
+                                            p_action VARCHAR(8) DEFAULT '',
+                                            p_id INTEGER DEFAULT 0,
+                                            p_basin VARCHAR(32) DEFAULT '',
+                                            p_abbr VARCHAR(4) DEFAULT ''
                                           ) 
-RETURNS TABLE
-    (
-        i_year INTEGER,
-        i_basin INTEGER, 
-        i_subject INTEGER, 
-        i_regime INTEGER,
-        i_user_ar INTEGER,
-        i_region INTEGER,
-        i_ar INTEGER,
-        i_unit INTEGER,
-        quota_volume NUMERIC(38,17),
-        fishery_volume NUMERIC(38,17)
-    )
+RETURNS void
 AS
 $$
-    DECLARE c_year INTEGER;
 BEGIN
-    -- запоминаем текущий год
-    c_year = q09.current_year( );
-    -- Год начала периода не ранее 2004 года и не позднее текущего года
-    IF p_year_from < 2004 OR p_year_from > c_year THEN
- 		RAISE EXCEPTION 'Year period-start should be between % and %', 2004, c_year;
-    END IF;
-    -- Год окончания периода не ранее начала периода и не позднее текущего года
-    IF  p_year_to < p_year_from OR p_year_to > c_year THEN
- 		RAISE EXCEPTION 'Year period-end should be between % and %', p_year_from, c_year;
-    END IF;
-        
-    RETURN QUERY
-        SELECT 
-                CAST(EXTRACT(YEAR FROM date_) AS INTEGER) AS n_year, id_basin, id_subject, 
-                id_regime, id_declarant, id_region, id_fish, id_unit, limits_volume,
-                q09.max(q09.max(catch_volume_stat, catch_volume_stat_oper), catch_volume_oper) as catch_volume
-            FROM q09.limits_catch
-            WHERE (portion_sign=0)AND(EXTRACT(YEAR FROM date_) BETWEEN p_year_from AND p_year_to);
-
+    -- insert new basin
+    IF p_action='CREATE' THEN
+    BEGIN
+        IF EXISTS(SELECT * FROM quotas.tb_basin) THEN
+            BEGIN
+                INSERT INTO quotas.tb_basin (id_basin, basin, basin_abbr)
+                            SELECT MAX(id_basin)+1, p_basin, p_abbr
+                            FROM quotas.tb_basin;
+                RETURN;
+            END;
+        ELSE
+            BEGIN
+                INSERT INTO quotas.tb_basin
+                    VALUES(1, p_basin, p_abbr);
+                RETURN;
+            END;
+        END IF;
+    END;    
+    END IF; 
+    -- update basin
+    IF p_action='PATCH' THEN
+        BEGIN
+            UPDATE quotas.tb_basin 
+                SET basin=p_basin, basin_abbr=p_abbr
+                WHERE id_basin=p_id;
+            RETURN;
+        END;
+    END IF; 
+    -- delete basin
+    IF p_action='REMOVE' THEN
+    	BEGIN
+            DELETE FROM quotas.tb_basin 
+                WHERE id_basin=p_id;
+            RETURN;
+        END;
+    END IF; 
 END;
 $$ LANGUAGE plpgsql;
+*/
 
+-- SELECT * FROM quotas.tb_basin;
+-- DELETE FROM quotas.tb_basin WHERE id_basin > 63;
+-- select quotas.sp_basin('CREATE', -1, 'Antarctic' );
+-- select quotas.sp_basin('CREATE', -1, 'Indian ocean', 'IO' );
+-- select quotas.sp_basin('CREATE', -1, 'Pacific ocean', 'PO' );
+-- select quotas.sp_basin('PATCH', 1, 'Atlantic ocean', 'AO' );
+-- select quotas.sp_basin('PATCH', 64, 'South ocean', 'SO' );
+-- select quotas.sp_basin('PATCH', 3, 'Pacific ocean', 'PO' );
+-- select quotas.sp_basin('PATCH', 65, 'North ocean', 'NO' );
+-- select quotas.sp_basin('REMOVE', 64 );
+-- select quotas.sp_basin('REMOVE', 65 );
--- db_quotas_2018/PostgreSQL/tables/catalog/tb_basin.sql
+++ db_quotas_2018/PostgreSQL/tables/catalog/tb_basin.sql
@@ -4,13 +4,18 @@
 -- PostgreSQL
 -- Автор: Иванченко М.В.
 -- Дата начала разработки:  28-03-2017
--- Дата обновления:         03-04-2017
+-- Дата обновления:         01-06-2017
 -- Первый релиз:            0.0.0.0
 -- Текущий релиз:           0.0.0.0
 -- =============================================================================
-CREATE TABLE IF NOT EXISTS tb_basin
+--
+DROP TABLE IF EXISTS quotas.tb_basin;
+--
+CREATE TABLE IF NOT EXISTS quotas.tb_basin
 (
-	id_basin INT NOT NULL PRIMARY KEY,			-- идентификатор бассейна промысла ВБР
-	basin VARCHAR(32) NOT NULL DEFAULT '',		-- наименование бассейна промысла ВБР
-	basin_abbr VARCHAR(4) NOT NULL DEFAULT ''	-- аббревиатура бассейна промысла ВБР
-);												
+	id_basin INT NOT NULL PRIMARY KEY,			    -- идентификатор бассейна промысла ВБР
+	basin VARCHAR(32) NOT NULL CHECK (basin <> ''),	-- наименование бассейна промысла ВБР
+	basin_abbr VARCHAR(4) NOT NULL DEFAULT ''	    -- аббревиатура бассейна промысла ВБР
+);
+-- 
+CREATE UNIQUE INDEX idx_basin ON quotas.tb_basin (basin);												
